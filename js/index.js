784a3b577e44efb88caa0aef9fc0c151ca327577
Iara <iburgueno@udesa.edu.ar>
2019-11-04 22:23:13 -0300

    Termine la pagina de generos y arregle el footer y el header del index

---
+++ js/index.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+  fetch("https://api.themoviedb.org/3/tv/top_rated?api_key=66f27e806074e1d0e5d2270f17e219a1&language=en-US&page=1")
+  .then(function(respuesta) {
+    return respuesta.json();
+  })
+  .then(function(informacion) {
+    var series = informacion.results;
+   console.log(series);
+
+   for (var i = 0; i < 6; i++) {
+
+     document.querySelector(".carrousel-populares ul").innerHTML += `
+     <li>
+         <div class="uk-card uk-card-default">
+             <div class="uk-card-media-top">
+                 <img style="height:390px!important; width: auto" src=https://image.tmdb.org/t/p/original/`+ series[i].poster_path +` alt="">
+             </div>
+             <div class="uk-card-body">
+                 <h3 class="uk-card-title tituloCarrousel">` + series[i].name + `</h3>
+                 <p>Puntaje: `+ series[i].vote_average +`</p>
+             </div>
+         </div>
+     </li>
+     `
+   }
+
+  })
+}
