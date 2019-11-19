fdf40ca5afc9c43464ce3ee8a5ac633982763656
Iara <iburgueno@udesa.edu.ar>
2019-11-19 01:06:07 -0300

    arreglos en el footer y modificaciones varias

--- js/resultados-buscador.js
+++ js/resultados-buscador.js
@@ -15,20 +15,40 @@ window.onload = function(){
     console.log(serie);
 
 
-    for (var i = 0; i < serie.length; i++) {
+    for (var i = 0; i < 10; i++) {
     if(serie[i].poster_path != null){
     document.querySelector(".seriestodas").innerHTML +=
 `
-<div class="serie-buscador"><div class="serie-imagen">
-      <a href="detalle-serie.html?idSerie=`+ serie[i].id + `"><img src=http://image.tmdb.org/t/p/w200` + serie[i].poster_path + `></a><h5>` + serie[i].name + `</h5>
-  </div>
-  `
+<div class="uk-animation-toggle serie-buscador" tabindex="0">
+  <a href="detalle-serie.html?idSerie=`+ serie[i].id + `">
+        <div class="uk-card uk-card-default uk-card-body uk-animation-slide-top-small serie-info">
+        <div class="serie-imagen">
+            <img src=http://image.tmdb.org/t/p/w200` + serie[i].poster_path + `>
+        </div>
+        <div class="serie-nombre">
+            <h5 class="uk-text-center serie-info"> ` + serie[i].name + `</h5>
+        </div>
+        </div>
+</a>
+</div>
+`
+
 } else {
   document.querySelector(".seriestodas").innerHTML +=
 `
-<div class="serie-buscador"><div class="serie-imagen">
-    <a href="detalle-serie.html?idSerie=`+ serie[i].id + `"><img src="imagenes/notfound.gif"></a><h5>` + serie[i].name + `</h5>
+<div class="uk-animation-toggle serie-buscador" tabindex="0">
+  <a href="detalle-serie.html?idSerie=`+ serie[i].id + `">
+        <div class="uk-card uk-card-default uk-card-body uk-animation-slide-top-small serie-info">
+        <div class="serie-imagen">
+            <img src="imagenes/notfound.gif">
+        </div>
+        <div class="serie-nombre">
+            <h5 class="uk-text-center tamano-letra"> ` + serie[i].name + `</h5>
+        </div>
+        </div>
+  </a>
 </div>
+
 `
 }
       }
@@ -40,7 +60,7 @@ window.onload = function(){
     console.log(error)
   })
 
-document.querySelector(".titulo-buscado").innerHTML += "<h1>Resultado de '"+ resultadosBuscador +"':</h1>"
+document.querySelector(".titulo-buscado").innerHTML += `<h1 class="titulo-buscado"> You search for '`+ resultadosBuscador +`':</h1>`
 
 
 }
